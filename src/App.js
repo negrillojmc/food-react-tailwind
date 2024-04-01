f63da935003a9dd769915e197882f618e7bf6add
chromatid10 <136996895+chromatid10@users.noreply.github.com>
2024-04-01 22:59:58 +0800

    Imported Food and Top Rated Menu sections

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Navbar from './Components/Navbar';
 import Hero from './Components/Hero';
+import HeadlineCards from './Components/HeadlineCards';
+import Food from './Components/Food';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <HeadlineCards />
+      <Food />
     </div>
   );
 }
