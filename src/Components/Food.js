47f734d8198c25c93b77afdf1b314f04b0701a5b
chromatid10 <136996895+chromatid10@users.noreply.github.com>
2024-04-02 02:31:46 +0800

    Created Food Section

--- src/Components/Food.js
+++ src/Components/Food.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {data} from '../Data/Data'
 
 const Food = () => {
-  console.log(data);
+  const [foods, setFoods] = useState(data)
   return (
     <div className='max-w-[1640px] mx-auto px-4 py-12'>
       <h1 className='text-orange-600 font-bold text-4xl text-center'>Top Rated Menu Items</h1>
@@ -32,6 +32,22 @@ const Food = () => {
             <button className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1'>$$$$</button>
           </div>
         </div>
+      </div>
+
+      {/* Display food selections */}
+      <div className='grid grid-cols-2 md:grid-cols-4 gap-6 pt-4'>
+        {foods.map((item, index) => (
+          <div className='border shadow-lg hover:scale-105 duration-300 rounded-lg' key={index}>
+            <img src={item.image} alt={item.name}
+            className='w-full h-[200px] object-cover rounded-t-lg' />
+            <div className='flex justify-between px-2 py-4'>
+              <p className='font-bold'>{item.name}</p>
+              <p>
+                <span className='bg-orange-500 text-white p-1 rounded-full'>{item.price}</span>
+              </p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   )
