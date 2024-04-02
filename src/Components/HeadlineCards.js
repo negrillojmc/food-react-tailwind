685e7a15cc6131a020a71e1a74797e14b6ef44b6
chromatid10 <136996895+chromatid10@users.noreply.github.com>
2024-04-02 17:30:29 +0800

    Edit headers

--- src/Components/HeadlineCards.js
+++ src/Components/HeadlineCards.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 const HeadlineCards = () => {
   return (
-    <div className='max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6'>
+    <div className='max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6 bg-orange-100'>
       {/* Card */}
       <div className='rounded-xl relative'>
         {/* Overlay */}
         <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
-            <p className='text-2xl font-bold px-2 pt-4'>Sun's Out, BOGO's Out</p>
-            <p className='px-2'>Through 1/12</p>
+            <p className='text-2xl font-bold px-2 pt-4'>Buy 1, Take 1</p>
+            <p className='px-2'>Through 5/30</p>
             <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
         </div>
         <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src='https://images.unsplash.com/photo-1624030997838-6b93e2d54d04?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='/' />
@@ -18,7 +18,7 @@ const HeadlineCards = () => {
       <div className='rounded-xl relative'>
         {/* Overlay */}
         <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
-            <p className='text-2xl font-bold px-2 pt-4'>New Restaurants</p>
+            <p className='text-2xl font-bold px-2 pt-4'>Asian Flavors</p>
             <p className='px-2'>Added Daily</p>
             <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
         </div>
@@ -29,8 +29,8 @@ const HeadlineCards = () => {
       <div className='rounded-xl relative'>
         {/* Overlay */}
         <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
-            <p className='text-2xl font-bold px-2 pt-4'>We Deliver Burgers Too</p>
-            <p className='px-2'>Tasty Treats</p>
+            <p className='text-2xl font-bold px-2 pt-4'>Best Burgers</p>
+            <p className='px-2'>Cheesy melt and more!</p>
             <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
         </div>
         <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src='https://images.unsplash.com/photo-1568901346375-23c9450c58cd?q=80&w=999&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3DD' alt='/' />
